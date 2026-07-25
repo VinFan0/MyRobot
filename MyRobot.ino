9ddfa6f6f82c8b80bce5777cc23df66c8bc39034
Ryan Beck <ryan.beck1820@gmail.com>
2026-07-25 16:25:07 -0600

    feat: bluetooth motor control
    Currently, bluetooth commands sent from the mobile application
    are just printed to the serial terminal. It also only accepts
    simple control mode with discrete characters and no range values.
    
    Add code to parse advanced mode bluetooth data and and store it in
    a global struct type, bt_readings_t.
    
    Also, the test motor is currently controlled by 2 push buttons for
    forward and reverse. Add code to enterpret the bt_readings_t struct
    and send PWM signals to the motors.

--- MyRobot.ino
+++ MyRobot.ino
@@ -1,24 +1,27 @@
-#include "oled.h"
-#include "motors.h"
-#include "servo.h"
 #include "bluetooth.h"
+#include "motors.h"
+#include "oled.h"
+#include "servo.h"
+
+bt_commands_t bt_readings;
 
 // ============================================================
 void setup() {
   Serial.begin(115200);
   delay(500); 
-  oled_init();
-  motors_init();
-  servo_init();
+  // oled_init();
+  // servo_init();
   bluetooth_init();
+  motors_init();
 }
 
 // ============================================================
 void loop() {
-  oled_update();
-  motors_update();
-  servo_update();
-  bluetooth_update();
+  // oled_update();
+  // servo_update();
+  bluetooth_update(&bt_readings);
+  motors_update(&bt_readings);
+  delay(100);
 }
 
 // ============================================================
--- motors.ino
+++ motors.ino
@@ -2,26 +2,90 @@
 
 void motors_init(void) {
   pinMode(LED_BUILTIN, OUTPUT);
-  pinMode(MOTORS[MOTOR_FORWARD], OUTPUT);
-  pinMode(MOTORS[MOTOR_REVERSE], OUTPUT);
-  pinMode(MOTOR_BUTTON_FORWARD, INPUT);
-  pinMode(MOTOR_BUTTON_REVERSE, INPUT);
-}
-void motors_update(void) {
-  if(digitalRead(MOTOR_BUTTON_FORWARD) == HIGH) {
-    digitalWrite(LED_BUILTIN, HIGH);
-    digitalWrite(MOTORS[MOTOR_FORWARD], HIGH);
-    digitalWrite(MOTORS[MOTOR_REVERSE], LOW);
-
-  } else if (digitalRead(MOTOR_BUTTON_REVERSE) == HIGH) {
-    digitalWrite(LED_BUILTIN, HIGH);
-    digitalWrite(MOTORS[MOTOR_FORWARD], LOW);
-    digitalWrite(MOTORS[MOTOR_REVERSE], HIGH);
-
-  } else {
-    digitalWrite(LED_BUILTIN, LOW);
-    digitalWrite(MOTORS[MOTOR_FORWARD], LOW);
-    digitalWrite(MOTORS[MOTOR_REVERSE], LOW);
-
+  
+  for(int i=0; i<NUM_MOTORS;) {
+    ledcAttach(MOTORS[i].forward_pin, MOTOR_PWM_FREQ, MOTOR_PWM_BITS);
+    ledcAttach(MOTORS[i].reverse_pin, MOTOR_PWM_FREQ, MOTOR_PWM_BITS);
+    motor_stop(i);
+    i++;
   }
+}
+
+void motors_update(bt_commands_t *bt_readings) {
+
+  // TODO: Scale motor inputs
+  // Max value from application is 99 + 60 = 159 (positive or negative)
+
+  /*
+  Right Motor
+  ------------
+  +Throttle(Forward)
+    +Yaw(Right)
+      Throttle - Yaw
+    -Yaw(Left)
+      Throttle - Yaw
+    
+    ** Throttle - Yaw**
+
+  -Throttle(Reverse)
+    +Yaw(Right)
+      Throttle - Yaw
+    -Yaw(Left)
+      Throttle - Yaw
+
+    ** Throttle - Yaw**
+
+  **********************************
+  +Throttle ---> Throttle - Yaw
+  -Throttle ---> -(Throttle - Yaw)
+  **********************************
+  */
+  float right_motor_command = abs(bt_readings->throttle_val) - bt_readings->yaw_val;
+
+  // Serial.print("Right Motor Value: ");
+  if (bt_readings->throttle_val > 0) { // +Throttle (Forward)
+    motor_forward(MOTOR_RIGHT_IDX, right_motor_command);
+    // Serial.print("Forward: ");
+
+  } else if (bt_readings->throttle_val < 0) {  // -Throttle (Reverse)
+    motor_reverse(MOTOR_RIGHT_IDX, right_motor_command);
+    // Serial.print("Reverse: ");
+
+  } else {  // 0 Throttle
+    if (bt_readings->yaw_val > 0) { // +Yaw (Right)
+      // Serial.print("Turn Right: ");
+      motor_reverse(MOTOR_RIGHT_IDX, bt_readings->yaw_val);
+
+    } else if (bt_readings->yaw_val < 0) { // -Yaw (Left)
+      // Serial.print("Turn Left: ");
+      motor_forward(MOTOR_RIGHT_IDX, abs(bt_readings->yaw_val));
+
+    } else { // Motors Stop
+      // Serial.print("Stop Motors");
+      motor_stop(MOTOR_RIGHT_IDX);
+    }
+  }
+  // Serial.println(right_motor_command);
+  
+}
+
+void motor_forward(int motor_idx, int speed) {
+  speed = constrain(map(speed, 39, 159, MOTOR_PWM_MIN, MOTOR_PWM_MAX), MOTOR_PWM_MIN, MOTOR_PWM_MAX);
+  Serial.print("Forward: ");
+  Serial.println(speed);
+  ledcWrite(MOTORS[motor_idx].forward_pin, speed);
+  ledcWrite(MOTORS[motor_idx].reverse_pin, 0);
+}
+
+void motor_reverse(int motor_idx, int speed) {
+  speed = constrain(map(speed, 39, 159, MOTOR_PWM_MIN, MOTOR_PWM_MAX), MOTOR_PWM_MIN, MOTOR_PWM_MAX);
+  Serial.print("Reverse: ");
+  Serial.println(speed);
+  ledcWrite(MOTORS[motor_idx].forward_pin, 0);
+  ledcWrite(MOTORS[motor_idx].reverse_pin, speed);
+}
+
+void motor_stop(int motor_idx) {
+  ledcWrite(MOTORS[motor_idx].forward_pin, 0);
+  ledcWrite(MOTORS[motor_idx].reverse_pin, 0);
 }
